68e770252f38a737bb5b38fbd2efa9fce2709751
Jacob Chademwiri <jchademwiri@gmail.com>
2021-10-05 13:10:35 +0200

    added more coins to 100

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home({ filteredCoins }) {
 
 export const getServerSideProps = async () => {
 	const res = await fetch(
-		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=20&page=1&sparkline=false'
+		'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 	);
 
 	const filteredCoins = await res.json();
