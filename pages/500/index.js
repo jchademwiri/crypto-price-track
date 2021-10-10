4fa9359406555450b96fca67bbf18f44bb52cdd7
Jacob <jchademwiri@gmail.com>
2021-10-10 14:53:54 +0200

    404 and 500

---
+++ pages/500/index.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+const NotFound500 = () => {
+	const router = useRouter();
+	useEffect(() => {
+		setTimeout(() => {
+			router.push('/');
+		}, 4000);
+	}, []);
+
+	return (
+		<div className='not-found'>
+			<h1>Page Not found</h1>
+			<p>
+				Redirecting to <Link href='/'>Home Page</Link>
+			</p>
+		</div>
+	);
+};
+
+export default NotFound500;
